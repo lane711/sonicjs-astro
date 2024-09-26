405c8b91980a6d35cc8acabf99e7857699bbe16a
Lane C <ldc0618@gmail.com>
2024-09-26 11:59:47 -0700

    add vitest dep

--- src/pages/api/v1/[table].ts
+++ src/pages/api/v1/[table].ts
@@ -2,8 +2,6 @@ export const prerender = false
 
 import type { APIRoute } from 'astro';
 
-const usernames = ["Sarah", "Chris", "Yan", "Elian"]
-
 export const GET: APIRoute = ({ params, request }) => {
   const table = params.table;
   return new Response(
@@ -12,23 +10,3 @@ export const GET: APIRoute = ({ params, request }) => {
     })
   )
 }
-
-export function getStaticPaths() {
-  return [
-    { params: { table: "contacts"} },
-    { params: { table: "posts"} },
-    { params: { table: "2"} },
-    { params: { table: "3"} }
-  ]
-}
-
-// export const prerender = false
-// import type { APIRoute } from 'astro'
-
-// export const GET: APIRoute = () => {
-//   return new Response(
-//     JSON.stringify({
-//       greeting: 'Hello',
-//     }),
-//   )
-// }
