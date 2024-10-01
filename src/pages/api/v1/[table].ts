ff96afdc8f341c340c48139bd790473f9d3a2da0
Lane C <ldc0618@gmail.com>
2024-10-01 13:29:24 -0700

    add data records

--- src/pages/api/v1/[table].ts
+++ src/pages/api/v1/[table].ts
@@ -5,8 +5,12 @@ import type { APIRoute } from "astro";
 import { getD1DataByTable } from "../../../services/d1-data";
 import { drizzle } from "drizzle-orm/d1";
 import { apiConfig, sonicJsConfig } from "../../../db/routes";
-import { tableSchemas, config } from "../../../db/routes";
-import { filterReadFieldAccess, getApiAccessControlResult, getItemReadResult } from "../../../auth/auth-helpers";
+import {
+  filterReadFieldAccess,
+  getApiAccessControlResult,
+  getItemReadResult,
+} from "../../../auth/auth-helpers";
+import { getRecords } from "../../../services/data";
 
 export const GET: APIRoute = async (context) => {
   const start = Date.now();
@@ -38,79 +42,78 @@ export const GET: APIRoute = async (context) => {
 
   // let data = await getD1DataByTable(env.D1, tableName, queryParams);
 
-
   if (entry.hooks?.beforeOperation) {
-    await entry.hooks.beforeOperation(ctx, "read", params.id);
+    await entry.hooks.beforeOperation(context, "read", params.id);
   }
 
-      const accessControlResult = await getApiAccessControlResult(
-        entry?.access?.operation?.read || true,
-        entry?.access?.filter?.read || true,
-        true,
-        ctx,
-        params.id,
-        entry.table
-      );
-
-      if (typeof accessControlResult === 'object') {
-        params.accessControlResult = { ...accessControlResult };
-      }
-
-      if (!accessControlResult) {
-        return ctx.text('Unauthorized', 401);
-      }
-
-      try {
-        params.limit = params.limit ?? '100';
-        // ctx.env.D1DATA = ctx.env.D1DATA;
-        let data = await getRecords(
-          ctx,
-          entry.table,
-          params,
-          ctx.req.url,
-          'fastest',
-          undefined
-        );
-
-        if (entry.access?.item?.read) {
-          const accessControlResult = await getItemReadResult(
-            entry.access.item.read,
-            ctx,
-            data
-          );
-          if (!accessControlResult) {
-            return ctx.text('Unauthorized', 401);
-          }
-        }
-        data.data = await filterReadFieldAccess(
-          entry.access?.fields,
-          ctx,
-          data.data
-        );
-
-        if (entry.hooks?.afterOperation) {
-          await entry.hooks.afterOperation(ctx, 'read', params.id, null, data);
-        }
-
-        const end = Date.now();
-        const executionTime = end - start;
-        // timerLog('api get', start, end);
-
-        // return ctx.json({ ...data, executionTime });
-        return new Response(
-          JSON.stringify({
-            data,
-            executionTime
-          })
-        );
-      } catch (error) {
-        console.log(error);
-        return ctx.text(error);
-      }
-
-  return new Response(
-    JSON.stringify({
-      data,
-    })
+  const accessControlResult = await getApiAccessControlResult(
+    entry?.access?.operation?.read || true,
+    entry?.access?.filter?.read || true,
+    true,
+    context,
+    params.id,
+    entry.table
   );
+
+  if (typeof accessControlResult === "object") {
+    params.accessControlResult = { ...accessControlResult };
+  }
+
+  if (!accessControlResult) {
+    return context.text("Unauthorized", 401);
+  }
+
+  try {
+    params.limit = params.limit ?? "100";
+
+    // let data = await getD1DataByTable(env.D1, tableName, queryParams);
+    let data = await getRecords(
+      context,
+      entry.table,
+      params,
+      context.request.url,
+      "fastest",
+      undefined
+    );
+
+    if (entry.access?.item?.read) {
+      const accessControlResult = await getItemReadResult(
+        entry.access.item.read,
+        context,
+        data
+      );
+      if (!accessControlResult) {
+        return context.text("Unauthorized", 401);
+      }
+    }
+    data.data = await filterReadFieldAccess(
+      entry.access?.fields,
+      context,
+      data.data
+    );
+
+    if (entry.hooks?.afterOperation) {
+      await entry.hooks.afterOperation(ctx, "read", params.id, null, data);
+    }
+
+    const end = Date.now();
+    const executionTime = end - start;
+    // timerLog('api get', start, end);
+
+    // return ctx.json({ ...data, executionTime });
+    return new Response(
+      JSON.stringify({
+        data,
+        executionTime,
+      })
+    );
+  } catch (error) {
+    console.log(error);
+    return new Response(
+      JSON.stringify({
+        error
+      }),
+      { status: 500 }
+    );
+  }
 };
--- src/services/data.ts
+++ src/services/data.ts
@@ -1,4 +1,4 @@
-// import { DrizzleD1Database, drizzle } from 'drizzle-orm/d1';
+import { DrizzleD1Database, drizzle } from 'drizzle-orm/d1';
 // import { v4 as uuidv4 } from 'uuid';
 // import { DefaultLogger, LogWriter, eq } from 'drizzle-orm';
 // import {
@@ -15,84 +15,85 @@
 //   getRecordFromKvCache,
 //   saveKVData
 // } from './kv-data';
-// import {
-//   deleteD1ByTableAndId,
-//   getD1ByTableAndId,
-//   getD1DataByTable,
-//   insertD1Data,
-//   updateD1Data
-// } from './d1-data';
-// import { log, timerLog } from '../util/logger';
+import {
+  deleteD1ByTableAndId,
+  getD1ByTableAndId,
+  getD1DataByTable,
+  insertD1Data,
+  updateD1Data
+} from './d1-data';
+import { log, timerLog } from '../services/logger';
+import { uuid } from './utils';
 
-// // export async function getRecordOld(d1, kv, id) {
-// //   const cacheKey = addCachePrefix(id);
-// //   const cacheResult = await getFromInMemoryCache(cacheKey);
-// //   console.log("cacheResult", cacheResult);
-// //   if (cacheResult && cacheResult.length) {
-// //     const cachedData = cacheResult[0].data;
-// //     console.log("**** cachedData ****", cachedData);
+// export async function getRecordOld(d1, kv, id) {
+//   const cacheKey = addCachePrefix(id);
+//   const cacheResult = await getFromInMemoryCache(cacheKey);
+//   console.log("cacheResult", cacheResult);
+//   if (cacheResult && cacheResult.length) {
+//     const cachedData = cacheResult[0].data;
+//     console.log("**** cachedData ****", cachedData);
 
-// //     return cachedData;
-// //   }
-// //   const kvData = await getRecordFromKvCache(kv, id);
+//     return cachedData;
+//   }
+//   const kvData = await getRecordFromKvCache(kv, id);
 
-// //   addToInMemoryCache(cacheKey, { data: kvData.data, source: "kv" });
+//   addToInMemoryCache(cacheKey, { data: kvData.data, source: "kv" });
 
-// //   return kvData;
-// // }
+//   return kvData;
+// }
 
-// // export async function getRecord(
-// //   d1,
-// //   kv,
-// //   table,
-// //   params,
-// //   cacheKey,
-// //   source = "fastest"
-// // ) {
-// //   const cacheStatusValid = await isCacheValid();
-
-// //   if (cacheStatusValid) {
-// //     const cacheResult = await getFromInMemoryCache(cacheKey);
-// //     // console.log("cacheResult", cacheResult);
-// //     if (cacheResult && cacheResult.length && source == "fastest") {
-// //       const cachedData = cacheResult[0].data;
-// //       // console.log("**** cachedData ****", cachedData);
-
-// //       return cachedData;
-// //     }
-// //   }
-
-// //   const kvData = await getRecordFromKvCache(kv, cacheKey);
-// //   if (source == "kv" || kvData) {
-// //     console.log("**** getting kv cache ****", kvData);
-// //     return kvData;
-// //   }
-
-// //   const d1Data = await getD1ByTableAndId(d1, table, params.id);
-
-// //   addToInMemoryCache(cacheKey, { data: d1Data.data, source: "cache" });
-// //   addToKvCache(kv, cacheKey, { data: d1Data.data, source: "kv" });
-
-// //   // console.log("sql results ==>", results);
-
-// //   return d1Data;
-// // }
-
-// export async function getRecords(
-//   ctx,
+// export async function getRecord(
+//   d1,
+//   kv,
 //   table,
 //   params,
 //   cacheKey,
-//   source = 'fastest',
-//   customDataFunction = undefined
-// ): Promise<{ data: any; source: string; total: number; contentType?: any }> {
-//   log(ctx, { level: 'verbose', message: 'getRecords start ' + cacheKey });
+//   source = "fastest"
+// ) {
+//   const cacheStatusValid = await isCacheValid();
+
+//   if (cacheStatusValid) {
+//     const cacheResult = await getFromInMemoryCache(cacheKey);
+//     // console.log("cacheResult", cacheResult);
+//     if (cacheResult && cacheResult.length && source == "fastest") {
+//       const cachedData = cacheResult[0].data;
+//       // console.log("**** cachedData ****", cachedData);
+
+//       return cachedData;
+//     }
+//   }
+
+//   const kvData = await getRecordFromKvCache(kv, cacheKey);
+//   if (source == "kv" || kvData) {
+//     console.log("**** getting kv cache ****", kvData);
+//     return kvData;
+//   }
+
+//   const d1Data = await getD1ByTableAndId(d1, table, params.id);
+
+//   addToInMemoryCache(cacheKey, { data: d1Data.data, source: "cache" });
+//   addToKvCache(kv, cacheKey, { data: d1Data.data, source: "kv" });
+
+//   // console.log("sql results ==>", results);
+
+//   return d1Data;
+// }
+
+export async function getRecords(
+  ctx,
+  table,
+  params,
+  cacheKey,
+  source = 'fastest',
+  customDataFunction = undefined
+): Promise<{ data: any; source: string; total: number; contentType?: any }> {
+  log(ctx, { level: 'verbose', message: 'getRecords start ' + cacheKey });
 
 //   const disableCache = ctx.env.disable_cache === 'true';
 //   const disableKv = ctx.env.disable_kv === 'true';
 //   console.log('disableCache / disableKv:', disableCache + ' / ' + disableKv);
 
-//   //cache
+  //cache
 //   if (!disableCache) {
 //     const cacheStart = Date.now();
 
@@ -129,7 +130,7 @@
 //     }
 //   }
 
-//   //kv
+  //kv
 //   if (!disableKv) {
 //     const kvStart = Date.now();
 
@@ -169,67 +170,48 @@
 //     }
 //   }
 
-//   var d1Data;
-//   let total = 0;
-//   if (customDataFunction) {
-//     log(ctx, {
-//       level: 'verbose',
-//       message: 'getRecords customDataFunction start'
-//     });
-//     d1Data = await customDataFunction();
-//     if (d1Data && d1Data[0]) {
-//       total = d1Data[0].total;
-//     } else if (Object.keys(d1Data).length) {
-//       total = 1;
-//     }
-//     log(ctx, {
-//       level: 'verbose',
-//       message: 'getRecords customDataFunction end'
-//     });
-//   } else {
-//     //d1
-//     const d1Start = Date.now();
+  var d1Data;
+  let total = 0;
+  if (customDataFunction) {
+    log(ctx, {
+      level: 'verbose',
+      message: 'getRecords customDataFunction start'
+    });
+    d1Data = await customDataFunction();
+    if (d1Data && d1Data[0]) {
+      total = d1Data[0].total;
+    } else if (Object.keys(d1Data).length) {
+      total = 1;
+    }
+    log(ctx, {
+      level: 'verbose',
+      message: 'getRecords customDataFunction end'
+    });
+  } else {
+    //d1
+    const d1Start = Date.now();
 
-//     log(ctx, {
-//       level: 'verbose',
-//       message: 'getRecords getD1DataByTable start'
-//     });
-//     d1Data = await getD1DataByTable(ctx.env.D1DATA, table, params);
-//     log(ctx, {
-//       level: 'verbose',
-//       message: 'getRecords getD1DataByTable end'
-//     });
-//     const d1End = Date.now();
-//     timerLog('d1 get', d1Start, d1End);
-//   }
+    log(ctx, {
+      level: 'verbose',
+      message: 'getRecords getD1DataByTable start'
+    });
+    d1Data = await getD1DataByTable(ctx.env.D1DATA, table, params);
+    log(ctx, {
+      level: 'verbose',
+      message: 'getRecords getD1DataByTable end'
+    });
+    const d1End = Date.now();
+    timerLog('d1 get', d1Start, d1End);
+  }
 
-//   if (d1Data?.length) {
-//     total = d1Data[0].total;
-//   } else if (d1Data) {
-//     total = 1;
-//     d1Data.total = undefined;
-//   }
+  if (d1Data?.length) {
+    total = d1Data[0].total;
+  } else if (d1Data) {
+    total = 1;
+    d1Data.total = undefined;
+  }
 
-//   // HACK to support int testing
-//   // if (executionCtx) {
-//   //   ctx.executionCtx.waitUntil(
-//   //     addToInMemoryCache(
-//   //       cacheKey,
-//   //       { data: d1Data, source: "cache", total },
-//   //       ctx.env.cache_ttl
-//   //     )
-//   //   );
-//   // } else {
-//   //   await addToInMemoryCache(
-//   //     cacheKey,
-//   //     {
-//   //       data: d1Data,
-//   //       source: "cache",
-//   //       total,
-//   //     },
-//   //     ctx.env.cache_ttl
-//   //   );
-//   // }
+
 //   if (!disableCache) {
 //     const cacheStart2 = Date.now();
 
@@ -243,7 +225,7 @@
 //     timerLog('cache add', cacheStart2, cacheEnd2);
 //   }
 
-//   //should  be  async
+  //should  be  async
 //   if (!disableKv) {
 //     const kvAddStart = Date.now();
 
@@ -279,104 +261,105 @@
 //     timerLog('kv add', kvAddStart, kvAddEnd);
 //   }
 
-//   log(ctx, { level: 'verbose', message: 'getRecords end', cacheKey });
-//   return { data: d1Data, source: 'd1', total };
-// }
+  log(ctx, { level: 'verbose', message: 'getRecords end', cacheKey });
+  return { data: d1Data, source: 'd1', total };
+}
 
-// async function dataAddToInMemoryCache(
-//   ctx,
-//   executionCtx,
-//   cacheKey,
-//   data,
-//   total
-// ) {
-//   // HACK to support int testing
+async function dataAddToInMemoryCache(
+  ctx,
+  executionCtx,
+  cacheKey,
+  data,
+  total
+) {
+  // HACK to support int testing
 
-//   if (executionCtx) {
-//     ctx.executionCtx.waitUntil(
-//       addToInMemoryCache(ctx, cacheKey, { data, source: 'cache', total })
-//     );
-//   } else {
-//     return addToInMemoryCache(ctx, cacheKey, { data, source: 'cache', total });
-//   }
-// }
+  if (executionCtx) {
+    ctx.executionCtx.waitUntil(
+      addToInMemoryCache(ctx, cacheKey, { data, source: 'cache', total })
+    );
+  } else {
+    return addToInMemoryCache(ctx, cacheKey, { data, source: 'cache', total });
+  }
+}
 
-// export async function insertRecord(d1, kv, data) {
-//   const content = data;
-//   const id = uuidv4();
-//   const timestamp = new Date().getTime();
-//   content.data.id = id;
-//   let error = '';
+export async function insertRecord(d1, kv, data) {
+  const content = data;
+  const id = uuid();
+  const timestamp = new Date().getTime();
+  content.data.id = id;
+  let error = '';
 
-//   // console.log("insertRecord", content);
-//   let result = {};
-//   try {
-//     result = await saveKVData(kv, id, content.data);
-//     // console.log('result KV', result);
-//     // return ctx.json(id, 201);
-//   } catch (error) {
-//     console.log('error', error);
-//     error = 'error posting content' + error;
-//   } finally {
-//     //then also save the content to sqlite for filtering, sorting, etc
-//     try {
-//       result = await insertD1Data(d1, kv, content.table, content.data);
-//       // console.log("insertD1Data --->", result);
-//       //expire cache
-//       await setCacheStatusInvalid();
-//       await clearKVCache(kv);
+  // console.log("insertRecord", content);
+  let result = {};
+  try {
+    result = await saveKVData(kv, id, content.data);
+    // console.log('result KV', result);
+    // return ctx.json(id, 201);
+  } catch (error) {
+    console.log('error', error);
+    error = 'error posting content' + error;
+  } finally {
+    //then also save the content to sqlite for filtering, sorting, etc
+    try {
+      result = await insertD1Data(d1, kv, content.table, content.data);
+      // console.log("insertD1Data --->", result);
+      //expire cache
+      await setCacheStatusInvalid();
+      await clearKVCache(kv);
 
-//       return { code: 201, data: result };
-//     } catch (error) {
-//       error =
-//         'error posting content ' +
-//         content.data.table +
-//         error +
-//         JSON.stringify(content.data, null, 2);
-//     }
-//   }
-//   return { code: 500, error };
-// }
+      return { code: 201, data: result };
+    } catch (error) {
+      error =
+        'error posting content ' +
+        content.data.table +
+        error +
+        JSON.stringify(content.data, null, 2);
+    }
+  }
+  return { code: 500, error };
+}
 
-// export async function updateRecord(d1, kv, data, params: Record<string, any>) {
-//   try {
-//     const result = await updateD1Data(d1, data.table, data, params);
-//     console.log('WTF WTF');
-//     if ('id' in result && result.id) {
-//       await saveKVData(kv, data.id, data);
-//     }
-//     //expire cache
-//     await setCacheStatusInvalid();
-//     await clearKVCache(kv);
-//     return { code: 200, data: result };
-//   } catch (error) {
-//     console.log('error posting content', error);
-//     return { code: 500, message: error };
-//   } finally {
-//     //then also save the content to sqlite for filtering, sorting, etc
-//     try {
-//       const result = await updateD1Data(d1, data.table, data);
-//       //expire cache
-//       await setCacheStatusInvalid();
-//       await clearKVCache(kv);
-//       return { code: 200, data: result };
-//     } catch (error) {
-//       console.log('error posting content', error);
-//     }
-//   }
-// }
+export async function updateRecord(d1, kv, data, params: Record<string, any>) {
+  try {
+    const result = await updateD1Data(d1, data.table, data, params);
+    console.log('WTF WTF');
+    if ('id' in result && result.id) {
+      await saveKVData(kv, data.id, data);
+    }
+    //expire cache
+    await setCacheStatusInvalid();
+    await clearKVCache(kv);
+    return { code: 200, data: result };
+  } catch (error) {
+    console.log('error posting content', error);
+    return { code: 500, message: error };
+  } finally {
+    //then also save the content to sqlite for filtering, sorting, etc
+    try {
+      const result = await updateD1Data(d1, data.table, data);
+      //expire cache
+      await setCacheStatusInvalid();
+      await clearKVCache(kv);
+      return { code: 200, data: result };
+    } catch (error) {
+      console.log('error posting content', error);
+    }
+  }
+}
 
-// export async function deleteRecord(d1, kv, data) {
-//   const timestamp = new Date().getTime();
+export async function deleteRecord(d1, kv, data) {
+  const timestamp = new Date().getTime();
 
-//   try {
-//     const kvResult = await deleteKVById(kv, data.id);
-//     const d1Result = await deleteD1ByTableAndId(d1, data.table, data.id);
+  try {
+    const kvResult = await deleteKVById(kv, data.id);
+    const d1Result = await deleteD1ByTableAndId(d1, data.table, data.id);
 
-//     await setCacheStatusInvalid();
-//     await clearKVCache(kv);
-//   } catch (error) {
-//     console.log('error deleting content', error);
-//     return { code: 500, message: error };
-//   }
-// }
+    await setCacheStatusInvalid();
+    await clearKVCache(kv);
+  } catch (error) {
+    console.log('error deleting content', error);
+    return { code: 500, message: error };
+  }
+}
+
