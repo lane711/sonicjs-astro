6744dfa2c574d6c17f7bb63878bf1afee2a4b600
Lane C <ldc0618@gmail.com>
2024-10-02 18:06:25 -0700

    table loading data

--- src/pages/admin/components/table.tsx
+++ src/pages/admin/components/table.tsx
@@ -8,60 +8,46 @@ import { useEffect, useState } from "react";
 
 const columnHelper = createColumnHelper();
 
-const data = [
+const fallbackData = [
   {
-    firstName: "Jane",
-    surname: "Doe",
-    age: 13,
-    gender: "Female",
-  },
-  {
-    firstName: "John",
-    surname: "Doe",
-    age: 43,
-    gender: "Male",
-  },
-  {
-    firstName: "Tom",
-    surname: "Doe",
-    age: 89,
-    gender: "Male",
+    id: "1",
+    title: "Record 1",
   },
 ];
 
 const columns = [
-  columnHelper.accessor((row) => `${row.firstName} ${row.surname}`, {
-    id: "fullName",
-    header: "Full Name",
+  // columnHelper.accessor((row) => `${row.firstName} ${row.surname}`, {
+  //   id: "fullName",
+  //   header: "Full Name",
+  // }),
+  columnHelper.accessor("id", {
+    header: "Id",
   }),
-  columnHelper.accessor("gender", {
-    header: "Gender",
+  columnHelper.accessor("title", {
+    header: "Title",
   }),
 ];
 
-function Table({tableName}) {
+function Table({ tableName }) {
   const [loading, setLoading] = useState(true);
   const [page, setPage] = useState(0);
+  const [data, setData] = useState(null);
 
   const pageSize = 18;
 
   const table = useReactTable({
-    data,
+    data: data ?? fallbackData,
     columns,
     getCoreRowModel: getCoreRowModel(),
   });
 
   useEffect(() => {
-    console.log('loading')
+    console.log("loading");
     setLoading(true);
 
     const offset = page * pageSize;
 
-
-    
-
     const originPath = `/api/v1/${tableName}?limit=${pageSize}&offset=${offset}`;
-
 
     getData(originPath);
   }, []);
@@ -69,56 +55,61 @@ function Table({tableName}) {
   const getData = (originPath) => {
     if (originPath) {
       fetch(`${originPath}`).then(async (response) => {
-
-        const data = await response.json();
-        console.log('data', data)
-        // setTotal(data.total);
-        // setUserData(data.data);
+        const {data} = await response.json();
+        console.log(data.data)
+        setData(data.data);
         setLoading(false);
       });
     }
   };
 
-  return (
-    <table>
-      <thead>
-        {table.getHeaderGroups().map((headerGroup) => {
-          return (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <th id={header.id}>
-                    {" "}
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext(),
-                        )}
-                  </th>
-                );
-              })}
-            </tr>
-          );
-        })}
-      </thead>
-      <tbody>
-        {table.getRowModel().rows.map((row) => {
-          return (
-            <tr key={row.id}>
-              {row.getVisibleCells().map((cell) => {
-                return (
-                  <td key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </td>
-                );
-              })}
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
-  );
+  if (table) {
+    return (
+      <table>
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => {
+            return (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <th id={header.id}>
+                      {" "}
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </th>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row) => {
+            return (
+              <tr key={row.id}>
+                {row.getVisibleCells().map((cell) => {
+                  return (
+                    <td key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    );
+  } else {
+    <div>no data yet</div>;
+  }
 }
 
 export default Table;
