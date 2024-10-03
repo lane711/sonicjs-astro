12a24acf996e2a2bb9efe4e276e7bd354b7125a9
Lane C <ldc0618@gmail.com>
2024-10-03 08:40:29 -0700

    table temp

--- src/pages/admin/components/table.tsx
+++ src/pages/admin/components/table.tsx
@@ -26,6 +26,9 @@ const columns = [
   columnHelper.accessor("title", {
     header: "Title",
   }),
+  columnHelper.accessor("updated_on", {
+    header: "Last Updated",
+  }),
 ];
 
 function Table({ tableName }) {
@@ -55,7 +58,7 @@ function Table({ tableName }) {
   const getData = (originPath) => {
     if (originPath) {
       fetch(`${originPath}`).then(async (response) => {
-        const {data} = await response.json();
+        const { data } = await response.json();
         setData(data.data);
         setLoading(false);
       });
@@ -64,47 +67,142 @@ function Table({ tableName }) {
 
   if (table) {
     return (
-      <table>
-        <thead>
-          {table.getHeaderGroups().map((headerGroup) => {
-            return (
-              <tr key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <th id={header.id}>
-                      {" "}
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext()
-                          )}
-                    </th>
-                  );
-                })}
-              </tr>
-            );
-          })}
-        </thead>
-        <tbody>
-          {table.getRowModel().rows.map((row) => {
-            return (
-              <tr key={row.id}>
-                {row.getVisibleCells().map((cell) => {
-                  return (
-                    <td key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </td>
-                  );
-                })}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <div className="bg-gray-900">
+        <div className="mx-auto max-w-7xl">
+          <div className="bg-gray-900 py-10">
+            <div className="px-4 sm:px-6 lg:px-8">
+              <div className="sm:flex sm:items-center">
+                <div className="sm:flex-auto">
+                  <h1 className="text-base font-semibold leading-6 text-white">
+                    {/* REACT {table} */}
+                  </h1>
+                  <p className="mt-2 text-sm text-gray-300">
+                    A list of all the users in your account including their
+                    name, title, email and role.
+                  </p>
+                </div>
+                <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+                  <button
+                    type="button"
+                    className="block rounded-md bg-indigo-500 px-3 py-2 text-center text-sm font-semibold text-white hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                  >
+                    Add user
+                  </button>
+                </div>
+              </div>
+              <div className="mt-8 flow-root">
+                <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                  <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                    <table>
+                      <thead>
+                        {table.getHeaderGroups().map((headerGroup) => {
+                          return (
+                            <tr key={headerGroup.id}>
+                              {headerGroup.headers.map((header) => {
+                                return (
+                                  <th id={header.id}>
+                                    {" "}
+                                    {header.isPlaceholder
+                                      ? null
+                                      : flexRender(
+                                          header.column.columnDef.header,
+                                          header.getContext()
+                                        )}
+                                  </th>
+                                );
+                              })}
+                            </tr>
+                          );
+                        })}
+                      </thead>
+                      <tbody>
+                        {table.getRowModel().rows.map((row) => {
+                          return (
+                            <tr key={row.id}>
+                              {row.getVisibleCells().map((cell) => {
+                                return (
+                                  <td key={cell.id}>
+                                    {flexRender(
+                                      cell.column.columnDef.cell,
+                                      cell.getContext()
+                                    )}
+                                  </td>
+                                );
+                              })}
+                            </tr>
+                          );
+                        })}
+                      </tbody>
+                    </table>
+
+                    <table className="min-w-full divide-y divide-gray-700">
+                      <thead>
+                        <tr>
+                          <th
+                            scope="col"
+                            className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-white sm:pl-0"
+                          >
+                            Name
+                          </th>
+                          <th
+                            scope="col"
+                            className="px-3 py-3.5 text-left text-sm font-semibold text-white"
+                          >
+                            Title
+                          </th>
+                          <th
+                            scope="col"
+                            className="px-3 py-3.5 text-left text-sm font-semibold text-white"
+                          >
+                            Email
+                          </th>
+                          <th
+                            scope="col"
+                            className="px-3 py-3.5 text-left text-sm font-semibold text-white"
+                          >
+                            Role
+                          </th>
+                          <th
+                            scope="col"
+                            className="relative py-3.5 pl-3 pr-4 sm:pr-0"
+                          >
+                            <span className="sr-only">Edit</span>
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody className="divide-y divide-gray-800">
+                        <tr>
+                          <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-0">
+                            Lindsay Walton
+                          </td>
+                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300">
+                            Front-end Developer
+                          </td>
+                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300">
+                            lindsay.walton@example.com
+                          </td>
+                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300">
+                            Member
+                          </td>
+                          <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                            <a
+                              href="#"
+                              className="text-indigo-400 hover:text-indigo-300"
+                            >
+                              Edit
+                              <span className="sr-only">, Lindsay Walton</span>
+                            </a>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   } else {
     <div>no data yet</div>;
