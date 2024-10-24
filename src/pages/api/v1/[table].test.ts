15a179dc90e551864c89d620553c5c76df870fc3
Lane C <ldc0618@gmail.com>
2024-10-24 09:37:11 -0700

    remove form method

--- src/pages/api/v1/[table].test.ts
+++ src/pages/api/v1/[table].test.ts
@@ -1,6 +1,6 @@
 
-// import { expect, test } from 'vitest'
-// import { GET } from './[table]'
+import { expect, test } from 'vitest'
+import { GET } from './[table]'
 
 // test('adds 1 + 2 to equal 3', () => {
 //   const result = GET({
