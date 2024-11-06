1d24cffb8b2f49ea4e22cc47dbc361ee6bf77158
Lane C <ldc0618@gmail.com>
2024-11-05 16:17:15 -0800

    cookie

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,27 +1,30 @@
 
 // import { lucia } from "./auth";
 import { defineMiddleware } from "astro:middleware";
-import { lucia } from "../auth/auth";
+// import { lucia } from "../auth/auth";
 
 export const onRequest = defineMiddleware(async (context, next) => {
-	const sessionId = context.cookies.get(lucia.sessionCookieName)?.value ?? null;
-	if (!sessionId) {
-		context.locals.user = null;
-		context.locals.session = null;
-		return next();
-	}
+    const env = context.locals.runtime.env;
 
-	const { session, user } = await lucia.validateSession(sessionId);
-	if (session && session.fresh) {
-		const sessionCookie = lucia.createSessionCookie(session.id);
-		context.cookies.set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
-	}
-	if (!session) {
-		const sessionCookie = lucia.createBlankSessionCookie();
-		context.cookies.set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
-	}
-	context.locals.session = session;
-	context.locals.user = user;
+    console.log('00> hello middelware', env);
+	// const sessionId = context.cookies.get(lucia.sessionCookieName)?.value ?? null;
+	// if (!sessionId) {
+	// 	context.locals.user = null;
+	// 	context.locals.session = null;
+	// 	return next();
+	// }
+
+	// const { session, user } = await lucia.validateSession(sessionId);
+	// if (session && session.fresh) {
+	// 	const sessionCookie = lucia.createSessionCookie(session.id);
+	// 	context.cookies.set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+	// }
+	// if (!session) {
+	// 	const sessionCookie = lucia.createBlankSessionCookie();
+	// 	context.cookies.set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+	// }
+	// context.locals.session = session;
+	// context.locals.user = user;
 	return next();
 });
 
