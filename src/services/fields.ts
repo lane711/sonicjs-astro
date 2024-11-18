13d89e4f94779ab8fb1055a1c5055877063a0188
Corey Jepperson <corey.jepperson@gmail.com>
2024-11-18 12:06:17 -0600

    typescript fixes

--- src/services/fields.ts
+++ src/services/fields.ts
@@ -1,4 +1,4 @@
-export const getFieldDefinitions = (tableConfig, record) => {
+export const getFieldDefinitions = (tableConfig: any, record?: any) => {
   const definitionObject = tableConfig.definition;
 
   // Convert the nested objects to an array
@@ -6,10 +6,10 @@ export const getFieldDefinitions = (tableConfig, record) => {
 
   const fields = Object.entries(tableConfig.fields ?? []);
 
-  const formFields = definitionArray.map((field) => {
+  const formFields = definitionArray.map((field: any) => {
     const key = field.config.name;
     const fieldOverride = tableConfig.fields ? tableConfig.fields[key] : null;
-    const fieldType = fieldOverride ? fieldOverride.type : "textField";
+    const fieldType = fieldOverride ? fieldOverride.type : 'textField';
 
     if (record && record[key]) {
       return { key, type: fieldType, value: record[key] };
